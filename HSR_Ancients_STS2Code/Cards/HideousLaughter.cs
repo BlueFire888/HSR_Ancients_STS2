9300c78057cfa07c22bda9e57a8cfa3ff99d3867
Michael Jones <mjj4@njit.edu>
2026-06-11 17:59:36 -0400

    added World's Funniest Joke relic. It almost works

---
+++ HSR_Ancients_STS2Code/Cards/HideousLaughter.cs
@@ -0,0 +1,59 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using HSR_Ancients_STS2.HSR_Ancients_STS2Code.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Powers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSR_Ancients_STS2.HSR_Ancients_STS2Code.Cards
+{
+    [Pool(typeof(CurseCardPool))]
+    public sealed class HideousLaughter : HSR_Ancients_STS2Card
+    {
+        public HideousLaughter() : base(2, CardType.Curse, CardRarity.Curse, TargetType.None)
+        {
+        }
+
+        public override bool CanBeGeneratedByModifiers => false;
+        public override int MaxUpgradeLevel => 0;
+
+        public override IEnumerable<CardKeyword> CanonicalKeywords
+        {
+            get
+            {
+                return (IEnumerable<CardKeyword>)new[] { CardKeyword.Eternal, CardKeyword.Ethereal};
+            }
+        }
+
+        protected override IEnumerable<IHoverTip> ExtraHoverTips
+        {
+           get
+            {
+               return (IEnumerable<IHoverTip>) [HoverTipFactory.FromPower<RingingPower>()];
+            }
+         
+        }
+       
+        public override bool HasTurnEndInHandEffect => true;
+        protected override async Task OnTurnEndInHand(PlayerChoiceContext choiceContext)
+        {
+            HideousLaughter cardSource = this;
+            PowerModel powerModel = (PowerModel) await PowerCmd.Apply<RingingPower>(choiceContext, cardSource.Owner.Creature, 1M, (Creature)null, (CardModel)cardSource);
+            if (powerModel == null)
+                return;
+            powerModel.SkipNextDurationTick = true;
+        }
+    }
+}
---
+++ HSR_Ancients_STS2Code/Relics/WorldsFunniestJoke.cs
@@ -0,0 +1,54 @@
+﻿using BaseLib.Utils;
+using HSR_Ancients_STS2.HSR_Ancients_STS2Code.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSR_Ancients_STS2.HSR_Ancients_STS2Code.Relics
+{
+
+
+[Pool(typeof(EventRelicPool))]
+    public sealed class WorldsFunniestJoke : HSR_Ancients_STS2Relic
+    {
+        public override RelicRarity Rarity => RelicRarity.Ancient;
+        public override bool HasUponPickupEffect => true;
+
+        protected override IEnumerable<IHoverTip> ExtraHoverTips
+        {
+            get
+            {
+                List<IHoverTip> items = new List<IHoverTip>();
+                items.Add(HoverTipFactory.ForEnergy((RelicModel)this));
+                items.AddRange(HoverTipFactory.FromCardWithCardHoverTips<HideousLaughter>());
+                return (IEnumerable<IHoverTip>) items;
+            }
+        }
+        protected override IEnumerable<DynamicVar> CanonicalVars
+        {
+            get
+            {
+                return new[] { (DynamicVar)new EnergyVar(1) };
+            }
+        }
+        public override async Task AfterObtained()
+        {
+            CardModel deck = await CardPileCmd.AddCurseToDeck<HideousLaughter>(this.Owner);
+        }
+
+        public override Decimal ModifyMaxEnergy(Player player, Decimal amount)
+        {
+            return player != this.Owner ? amount : amount + this.DynamicVars.Energy.BaseValue;
+        }
+    }
+}
